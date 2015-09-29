030d6deb68391c5e3f6e6f08d499b4f1a5f8897e
unknown <145003371@mvhsrop901-05.vusdnoc.vusd.k12.ca.us>
2015-09-29 13:35:27 -0700

    commit 2

--- src/school/Person.java
+++ src/school/Person.java
@@ -1,98 +1,103 @@
-
 package school;
 import java.util.ArrayList;
-
-public class Person {
-    
-//    private static int numPeople = 10;
-//    private static int currentPeopleIndex = 0;
-//    private static Person people[] = new Person[numPeople];
-    private static ArrayList<Person>people = new ArrayList<Person>();
-    
-    enum Gender{
-        Male, Female
+import java.util.Calendar;
+public class Person {      
+    protected static ArrayList<Person> people = new ArrayList<Person>();
+    enum Gender {
+        Male,Female
     }
- 
     private String name;
     private int weight;
     private Gender gender;
-    
-    public static Person addPerson(String _name, Gender _gender, int _weight)
+    private int birthDay;
+    private int birthMonth;
+    private int birthYear;
+
+    public static Person addPerson(String _name,
+    Gender _gender, int _weight)
     {
-        Person temp = new Person(_name, _gender, _weight);
- //       people[currentPeopleIndex++] = temp;
+        Person temp = new Person(_name,_gender,_weight);
         people.add(temp);
         return(temp);
-    }
-    
+    }    
+    public static void addPerson(Person _person)
+    {
+        people.add(_person);
+    }    
     Person()
     {
-       name ="Obama";
-       weight = 150;
-       gender = Gender.Male;
+        name = "None";
+        weight = 100;
+        gender = Gender.Female;
     }
-    
     Person(String _name,Gender _gender,int _weight)
     {
         name = _name;
-       weight = _weight;
-       gender = _gender;
-    }
-    
-    public int getWeight()
+        weight = _weight;
+        gender = _gender;
+    }  
+    public void setBirthdate
+    (int _day,int _month,int _year)
     {
-        return(weight);
+        birthDay = _day;
+        birthMonth = _month;
+        birthYear = _year;
+          
     }
+    public int getAge()
+    {
+        Calendar now = Calendar.getInstance();
+        int day = now.get(Calendar.DAY_OF_MONTH);
+        int month = now.get(Calendar.MONTH) + 1;
+        int year = now.get(Calendar.YEAR);
+        return(0);
+    }    
     public void setWeight(int _weight)
     {
         weight = _weight;
     }
-    public String getName()
+    public int getWeight()
     {
-        return(name);
-    }
+        return(weight);
+    }       
     public void setName(String _name)
     {
         name = _name;
     }
-    public Gender getGender()
+    public String getName()
     {
-        return(gender);
-    }
+        return(name);
+    }    
     public void setGender(Gender _gender)
     {
         gender = _gender;
     }
+    public Gender getGender()
+    {
+        return(gender);
+    }      
     
-    public static void printName()
+    public static void printNames()
     {
         System.out.println("===printNames===");
-//        for(int i = 0;i < numPeople;i++)
-//        if(people[i] != null)
-//        {
-//            System.out.println(people[i].getName());
-//        }
-//                for(int i = 0;i < people.size();i++)
-//            System.out.println(people.get(i).getName());
-                
-          for(Person temp : people)
-          {
-              System.out.println(temp.getName());
-          }
-    }
-    
-    public static void printName(Gender _gender)
+        for (Person temp : people)
+        {
+            System.out.println(temp.getName());
+        }
+    }    
+    public static void printNames(Gender _gender)
     {
         System.out.println("===printNames===");
-        for(Person temp : people)
-          {
-              if(temp.gender == _gender)
-              System.out.println(temp.getName());
-          }
+        for (Person temp : people)
+        {
+            if (temp.gender == _gender)
+                System.out.println(temp.getName());
+        }
     }
+    
     
     public String toString()
     {
         return(name + " " + gender + " " + weight);
-    }
+    }    
 }
